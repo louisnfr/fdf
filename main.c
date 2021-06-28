48a07b3e10eec67e871391d379b0c7b8e7d2cc58
Louis Raffin <louis.raffin@gmail.com>
2021-06-29 01:45:19 +0200

    window

--- main.c
+++ main.c
@@ -6,18 +6,21 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 21:12:31 by lraffin           #+#    #+#             */
-/*   Updated: 2021/06/29 01:25:23 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/06/29 01:44:32 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx_macos/mlx.h"
-#include "minilibx_macos/mlx_int.h"
-#include "minilibx_macos/mlx_new_window.h"
+#include "mlx.h"
 
 int	main()
 {
 	void	*mlx_ptr;
+	void	*win_ptr;
+
 	mlx_ptr = mlx_init();
-	mlx_new_window(mlx_ptr, 500, 500, "test MLX");
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "test MLX");
+	mlx_loop(mlx_ptr);
+
+	// mlx_pixel_put();
 	return (0);
 }
