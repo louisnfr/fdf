2cd443ffb5db30bc430993fe7fd301116b35af18
louisnfr <louis.raffin@gmail.com>
2021-09-21 22:10:35 +0200

    fdf

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 21:12:31 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/21 21:45:46 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/21 22:10:11 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,26 @@ void	ft_iso(t_map *map, t_point *a)
 		+ ((*a).x + (*a).y) * sin(map->angle);
 }
 
+void	usage(void)
+{
+	write(2, "usage: ./fdf [file]\n", 20);
+	exit(EXIT_FAILURE);
+}
+
 int	main(int ac, char **av)
 {
 	t_map	*map;
 
 	errno = 0;
+	if (ac != 2)
+		usage();
 	map = malloc(sizeof(t_map));
 	if (!map)
 		ft_terminate(ERR_MALLOC, map);
 	ft_init(map);
-	if (ac == 2)
-	{
-		map->file = av[1];
-		ft_parse(map);
-		ft_fdf(map);
-	}
+	map->file = av[1];
+	ft_parse(map);
+	ft_fdf(map);
 	free(map);
 	return (0);
 }
